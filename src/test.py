4a24041ea448ddae83d5eb7c30b8754ab97b3313
u-tain <korchevnuktc@gmail.com>
2023-06-26 00:04:01 +0300

    add report

--- src/test.py
+++ src/test.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import f1_score, accuracy_score
 import torch
 import tqdm
+from preprocess import read_vocab
+from model import CNN_LSTM
 
 def make_test(model,dataloader,device):
     all_true_labels = []
@@ -22,5 +24,18 @@ def make_test(model,dataloader,device):
     acc_list.append(epoch_acc.tolist())
     return all_true_labels,all_preds
 
-# if __name__ == "__main__":
-    # загружаем словарь, загружаем данные 
+if __name__ == "__main__":
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')  
+
+    # загружаем словарь 
+    vocab_lables = read_vocab('data/vocab_labels.txt')
+    vocab = read_vocab('data/vocab.txt')
+
+    # загружаем данные
+
+    # загружаем модель
+    model = CNN_LSTM(len(vocab), n_classes = len(vocab_lables)).to(device)
+    model.load_state_dict(torch.load('weight/cnn_lstm.pth'))
+    
+
